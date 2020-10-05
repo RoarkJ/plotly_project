5c65cfa6b9f4f6dd8523d1f0bf692e4900f9d061
Bryan <30875957+RoarkJ@users.noreply.github.com>
2020-10-04 22:02:24 -0600

    add the bubble plot

--- static/js/app.js
+++ static/js/app.js
@@ -18,8 +18,10 @@ function selectMetaData(subject) {
         var firstSubject = filteredSel[0];
         console.log(firstSubject);
         var starterData = d3.select("#sample-metadata");
+        //clear list
+        starterData.html("");
         Object.entries(firstSubject).forEach(([key, value])=> {
-            starterData.append("p").text(`${key}${value}`)
+            starterData.append("p").text(`${key}: ${value}`)
         });
     });
 };
@@ -27,19 +29,20 @@ function selectMetaData(subject) {
 function optionChanged(subject) {
     selectMetaData(subject)
     plotData(subject)
+    plotBubble(subject)
 };
 
 function plotData(subject) {
     d3.json("samples.json").then((data)=> {
         var samples = data.samples;
-        var sampSel = samples.filter(samplesObj=>samplesObj.id== subject);
+        var sampSel = samples.filter(samplesObj=>samplesObj.id == subject);
         var firstSample = sampSel[0];
         console.log(firstSample);
 
         var trace = {
             type: "bar",
             orientation: "h",
-            x: firstSample.sample.sample_values.slice.reverse(),
+            x: firstSample.sample_values.slice(0, 10).reverse(),
             y: firstSample.otu_ids.map(id => `otu_id ${id}`).slice(0, 10).reverse(),
             text: firstSample.otu_labels.slice(0, 10).reverse()
         };
@@ -51,6 +54,35 @@ function plotData(subject) {
         Plotly.newPlot("bar", bar_data, layout);
     });
 
+}
+
+function plotBubble(subject) {
+    d3.json("samples.json").then((data)=> {
+        var samples = data.samples;
+        var sampSel = samples.filter(samplesObj=>samplesObj.id == subject); 
+        var firstSample = sampSel[0];
+        console.log(firstSample);
+
+        var trace = {
+            type: "scatter",
+            x: firstSample.otu_ids,
+            y: firstSample.sample_values,
+            mode: "markers",
+            text: firstSample.otu_labels,
+            marker: {
+                colorscale: "Earth",
+                color: firstSample.otu_ids,
+                size: firstSample.sample_values.map((sample) => sample/1.5)
+            }
+        };
+        var bubble_data = [trace];
+
+        var layout = {
+            xaxis: {title: "OTU_ID"}
+        };
+
+        Plotly.newPlot("bubble", bubble_data, layout);
+    });
 }
 
 selectBiodiv();
